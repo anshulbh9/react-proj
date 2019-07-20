0ca327f8f5681f5a037aa7652e491341cb681e2d
anshulbh9 <anshulbh9@gmail.com>
2019-07-20 22:23:12 +0530

    changes

---
+++ src/components/AboutPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <>
+        <h2>About</h2>
+        <p> this a react tutorial</p>
+      </>
+    );
+  }
+}
+export default AboutPage;
---
+++ src/components/HomePage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+function HomePage() {
+  return (
+    <div className="jumbotron">
+      <h1>Puluralsight Admistration</h1>
+      <p>React flux,react router fron developement</p>
+      <Link to="about" className="btn btn-primary">
+        {" "}
+        About
+      </Link>
+    </div>
+  );
+}
+export default HomePage;
